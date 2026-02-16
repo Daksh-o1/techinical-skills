aa584aaccb685ae1c2561774efcd2a61885e2d4c
Daksh-o1 <dakshverma7898@gmail.com>
2026-02-16 10:48:01 +0530

    sum of core elements.

--- sum_of_cores.java
+++ sum_of_cores.java
@@ -1,14 +1,9 @@
+//wap to find sum of core elements of a matrix
 
-// sum of frame elements
 import java.util.Scanner;
 
-public class sum_of_frame_ele {
+public class sum_of_cores {
     public static void main(String[] args) {
-        // int matrix[][] = {
-        //         { 1, 2, 3 },
-        //         { 4, 5, 6 },
-        //         { 7, 8, 9 }
-        // };
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter row size: ");
         int row = sc.nextInt();
@@ -24,16 +19,14 @@ public class sum_of_frame_ele {
         int sum = 0;
         for (int i = 0; i < row; i++) {
             for(int j = 0;j<cols;j++){
-                if(i==0||j==0||i==row-1||j==cols-1){
+                if(i!=0 && j!=0 && i!=row-1 && j!=cols-1){
                     sum += matrix[i][j];
-                    // System.out.print(matrix[i][j]+" ");
-                }
-                else{
-                    System.out.println("core elements: - ");
-                    System.out.print(matrix[i][j]+ " ");
+                    // System.out.println("Core elements: ");
+                    System.out.print(matrix[i][j]+" ");
                 }
             }
             System.out.println();
+        
         }
 
         System.out.println("sum: "+ sum);
@@ -41,4 +34,3 @@ public class sum_of_frame_ele {
         sc.close();
     }
 }
-
